7c9bd976b1ba99c5b3a8365b0ea5c6a44de48a13
Sasha <sashalapushkina@mail.ru>
2022-05-01 14:31:13 +0300

    FIX

--- src/GaussSeidel.java
+++ src/GaussSeidel.java
@@ -8,9 +8,8 @@ public class GaussSeidel {
     private int m; //кол-во коэффициентов (столбцов)
     private double[][] matrix; //коэффициенты
     private double[] solution;
-    private int count; //кол-во итераций
-    private final double accuracy = 1E-1;
-    private boolean SCC = true;
+    private int[] order;
+    private static final double EPS = 1E-3;
 
     //выделение памяти под массив
     private void create(int n, int m) {
@@ -27,47 +26,153 @@ public class GaussSeidel {
         String[] sn = pat.split(str);
         n = Integer.parseInt(sn[0]);
         m = n + 1;
-        count = Integer.parseInt(sn[1]);
         solution = new double[n];
         create(n, m);
-        int i, j;
-        for (i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) {
             str = scan.nextLine();
             sn = pat.split(str);
-            for (j = 0; j < m; j++)
+            for (int j = 0; j < m; j++)
                 matrix[i][j] = Double.parseDouble(sn[j]);
         }
+        order = new int[n];
+        for (int i = 0; i < n; i++) {
+            order[i] = i;
+        }
         scan.close();
+    }
+
+    private double get(int i, int j){
+        return matrix[order[i]][j];
     }
 
     //вывод системы на печать
     public void print() {
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                System.out.printf("%15.6E", matrix[i][j]);
+                System.out.printf("%15.6E", get(i ,j));
             }
             System.out.println();
         }
     }
 
+    //проверяем нули на главной диагонали
+    public boolean areThereZerosOnDiagonal() {
+        for (int i = 0; i < n; i++) {
+            if (Math.abs(get(i ,i)) < EPS) //если на диагонали 0
+                return true;
+        }
+        return false;
+    }
+
+    //переставить строки
+    public boolean rearrangeRows() {
+        int[] permutation = new int[n];
+        for (int i = 0; i < n; i++) {
+            permutation[i] = i;
+        }
+
+        allPermutationRecursive(0, permutation);
+        return areThereZerosOnDiagonal();
+    }
+
+    // Перебор всех возможных перестановок строк
+    private boolean allPermutationRecursive(int index, int[] elements) {
+        if (index == n - 1) {
+            if (!(Math.abs(matrix[elements[index]][index]) < EPS)) {
+                order = elements;
+                return isSCC();
+            }
+        } else {
+            for (int i = index + 1; i < n; i++) {
+                if (Math.abs(matrix[elements[index]][index]) < EPS) {
+                    swap(elements, index, i);
+                    continue;
+                }
+
+                if (allPermutationRecursive(index + 1, elements)) {
+                    return true;
+                }
+
+                swap(elements, index, i);
+            }
+
+            if (!(Math.abs(matrix[elements[index]][index]) < EPS)) {
+                return allPermutationRecursive(index + 1, elements);
+            }
+        }
+
+        return false;
+    }
+
+    private void swap(int[] input, int a, int b) {
+        int tmp = input[a];
+        input[a] = input[b];
+        input[b] = tmp;
+    }
+
+    // Проверка ДУС для матрицы
+    public boolean isSCC() {
+        boolean isSCC = false;
+        for (int i = 0; i < n; i++) {
+            switch (isRowSCC(order[i], i)) {
+                case 0:
+                    isSCC = true;
+                case 1:
+                    break;
+                case 2:
+                    return false;
+            }
+        }
+        return isSCC;
+    }
+
+    // Проверка достаточного условия сходимости для строки
+    private int isRowSCC(int rowId, int diagonalId) {
+        double mainElement = 0.0;
+        double allElement = 0.0;
+
+        for (int i = 0; i < n; i++) {
+            if (i == diagonalId) {
+                mainElement += Math.abs(matrix[rowId][i]);
+            } else {
+                allElement += Math.abs(matrix[rowId][i]);
+            }
+        }
+
+        if (mainElement > allElement + EPS) {
+            return 0;                                   // ДУС выполнена в полном объёме для строки (>)
+        } else if (mainElement > allElement - EPS) {
+            return 1;                                   // ДУС выполнена в неполном объёме для строки (>=)
+        } else {
+            return 2;                                   // ДУС невыполнена для строки (<)
+        }
+    }
+
     //расчет решения
-    public int resolve() {
-        System.out.println("Решения:");
+    public double[] resolve(double accuracy) {
+        double difference = iterate();
+        while (difference > accuracy) {
+            difference = iterate();
+        }
+        return solution;
+    }
+
+    public double[] resolve(double accuracy, int count, int limit) {
+        int decreaseCount = 0;
         double difference = iterate();
         for (int i = 1; i < count; i++) {
             double newDifference = iterate();
-            if (newDifference > difference) { //проверяем монотонное убывание на каждом шаге (?)
-                return 2;
-            }
-            if (newDifference < accuracy) { //выходим из цикла, если добились нужной точности (?)
-                return 0;
+            if (newDifference < difference) //проверяем монотонное убывание на каждом шаге
+                decreaseCount++;
+            else
+                decreaseCount = 0;
+            if (newDifference < accuracy) { //выходим из цикла, если добились нужной точности
+                return solution;
             }
             difference = newDifference;
         }
-        while (SCC && difference > accuracy) {
-            difference = iterate();
-        }
-        return 0;
+        if (decreaseCount < limit) return null;
+        else return resolve(accuracy);
     }
 
     //итерация поиска решения
@@ -75,19 +180,17 @@ public class GaussSeidel {
         double[] result = new double[n];
         double maxDifference = Double.MIN_VALUE;
         for (int i = 0; i < n; i++) {
-            result[i] = solveVariable(i);
-            double difference = abs(result[i] - solution[i]);
+            result[i] = solveVariable(order[i]);
+            double difference = Math.abs(result[i] - solution[i]);
             if (difference > maxDifference)
                 maxDifference = difference;
         }
         solution = result;
-        solution();
-        System.out.println();
         return maxDifference;
     }
 
     //выразить переменную
-    private double solveVariable(int index){
+    private double solveVariable(int index) {
         double sum = 0;
         for (int i = 0; i < n; i++) {
             if (i != index)
@@ -96,62 +199,8 @@ public class GaussSeidel {
         return (matrix[index][m - 1] - sum) / matrix[index][index];
     }
 
-    //привести матрицу к диагональному преобладанию
-    public int diagonal() {
-        for (int i = 0; i < n; i++) { //сверху вниз
-            if (matrix[i][i] == 0) { //если на диагонали 0
-                int row = findTransposition(i);
-                if (row == -1) return 1;
-                else swap(i, row);
-            }
-        }
-        return 0;
-    }
-
-    //найти перестановку в соответствии с ДУС
-    private int findTransposition(int start) {
-        int flag = -1;
-        for (int i = start; i < n; i++) {
-            if (matrix[i][start] != 0) {
-                if (checkSCC(i, start))
-                    return i;
-                else flag = i;
-            }
-        }
-        SCC = false;
-        return flag;
-    }
-
-    //проверить строку на ДУС
-    private boolean checkSCC(int row, int main) {
-        double sum = 0;
-        for (int i = 0; i < n; i++) {
-            if (i != main) {
-                if (matrix[row][i] > matrix[row][main])
-                    return false;
-                else sum += matrix[row][i];
-            }
-        }
-        return matrix[row][main] >= sum;
-    }
-
-    //модуль числа
-    private double abs(double a) {
-        return a >= 0 ? a : -a;
-    }
-
-    //поменять местами строки a и b
-    private void swap(int a, int b) {
-        if (a != b)
-            for (int i = 0; i < m; i++) {
-                double temp = matrix[a][i];
-                matrix[a][i] = matrix[b][i];
-                matrix[b][i] = temp;
-            }
-    }
-
     //вывод решения
-    public void solution() {
+    public void printSolution() {
         for (int i = 0; i < n; i++) {
             System.out.printf("%15.6E", solution[i]);
         }
--- src/Main.java
+++ src/Main.java
@@ -3,28 +3,34 @@ import java.io.FileNotFoundException;
 public class Main {
     public static void main(String[] args) {
         GaussSeidel gaussSeidel = new GaussSeidel();
-        try
-        {
+        try {
             gaussSeidel.init("input.txt");
-        }
-        catch (FileNotFoundException e)
-        {
+        } catch (FileNotFoundException e) {
             System.out.println("File Not Found!");
         }
 
         System.out.println("Система:");
         gaussSeidel.print();
-        int code = gaussSeidel.diagonal();
-        if (code == 1) System.out.println("Невозможно решить итерационным методом");
-        else {
-            System.out.println("Система после перестановок:");
-            gaussSeidel.print();
-            code = gaussSeidel.resolve();
-            if (code == 2) System.out.println("Метод расходится");
+
+        if (gaussSeidel.areThereZerosOnDiagonal()) {
+            if (gaussSeidel.rearrangeRows()) {
+                System.out.println("Невозможно решить итерационным методом");
+                return;
+            }
             else {
-                System.out.print("Ответ: ");
-                gaussSeidel.solution();
+                System.out.println("Система после перестановок:");
+                gaussSeidel.print();
             }
         }
+        if (gaussSeidel.isSCC())
+            gaussSeidel.resolve(1E-2);
+        else {
+            if (gaussSeidel.resolve(1E-2, 10, 2) == null) {
+                System.out.println("Метод расходится");
+                return;
+            }
+        }
+        System.out.print("Ответ: ");
+        gaussSeidel.printSolution();
     }
 }
